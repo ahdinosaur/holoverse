6b3c77f4297d525d5d022ae8970b23798fa53edd
Michael Williams <dinosaur@riseup.net>
2013-09-03 17:50:18 -0700

    made basic hives view based on example data

--- client/workshift.js
+++ client/workshift.js
@@ -1,11 +1,83 @@
-Template.hello.greeting = function () {
-  return "Welcome to workshift.";
-};
+Router.configure({
+  layout: 'layout',
+  notFoundTemplate: 'notFound',
+  loadingTemplate: 'loading'
+});
 
-Template.hello.events({
-  'click input' : function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
+Router.map(function () {
+  this.route('home', {
+    path: '/'
+  });
+
+  this.route('hives', {
+    controller: 'HivesController'
+  });
+});
+
+HivesController = RouteController.extend({
+  template: 'hives',
+  waitOn: function () {
+    return Meteor.subscribe('hives');
+  },
+  data: function () {
+    return {
+      hives: Hives.find()
+    };
+  },
+  run: function () {
+    this.render('hives');
   }
 });
+
+var hivesTree = function () {
+  var getChildren = function (hive) {
+    if (hive.children) {
+      hive.children = hive.children.map(function (id) {
+        return getChildren(Hives.findOne({ _id: id }));
+      });
+    }
+    return hive
+  };
+  var hives = [];
+  Hives.find({parent: null}).forEach(function (rootHive) {
+    hives.push(getChildren(rootHive));
+  });
+  console.log(JSON.stringify(hives, null, 2));
+  return hives;
+}
+
+Template.hives.rendered = function () {
+  var diameter = 960,
+      format = d3.format(",d");
+
+  var pack = d3.layout.pack()
+      .size([diameter - 4, diameter - 4])
+      .value(function(d) { return 1 });//d.size; });
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", diameter)
+      .attr("height", diameter)
+      .append("g")
+      .attr("transform", "translate(2,2)");
+
+  hivesTree().forEach(function (root) {
+    var node = svg.datum(root).selectAll(".node")
+        .data(pack.nodes)
+        .enter().append("g")
+        .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    node.append("title")
+        .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
+
+    node.append("circle")
+        .attr("r", function(d) { return d.r; });
+
+    node.filter(function(d) { console.log(d, d.children); return !d.children; }).append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.name.substring(0, d.r / 3); });
+  });
+  
+  d3.select(self.frameElement).style("height", diameter + "px");
+}
--- server/workshift.js
+++ server/workshift.js
@@ -1,3 +1,50 @@
 Meteor.startup(function () {
   // code to run on server at startup
+  Hives.remove({});
+  var mikey_id = Hives.insert({
+    name: 'Mikey'
+  });
+  var daniel_id = Hives.insert({
+    name: 'Daniel'
+  });
+  var ernest_id = Hives.insert({
+    name: 'Ernest'
+  });
+  var peter_id = Hives.insert({
+    name: 'Peter'
+  });
+  var cloyne_id = Hives.insert({
+    name: 'Cloyne'
+  });
+  var davis_id = Hives.insert({
+    name: 'Davis'
+  });
+  var bsc_id = Hives.insert({
+    name: 'BSC'
+  });
+  Hives.update(mikey_id, {
+    $set: { parent: [cloyne_id] }
+  });
+  Hives.update(daniel_id, {
+    $set: { parent: [cloyne_id] }
+  });
+  Hives.update(ernest_id, {
+    $set: { parent: [davis_id] }
+  });
+  Hives.update(peter_id, {
+    $set: { parent: [davis_id] }
+  });
+  Hives.update(cloyne_id, {
+    $set: { parent: [bsc_id], children: [mikey_id, daniel_id] }
+  });
+  Hives.update(davis_id, {
+    $set: { parent: [bsc_id], children: [ernest_id, peter_id] }
+  });
+  Hives.update(bsc_id, {
+    $set: { children: [cloyne_id, davis_id]}
+  })
+});
+
+Meteor.publish('hives', function () {
+  return Hives.find();
 });
